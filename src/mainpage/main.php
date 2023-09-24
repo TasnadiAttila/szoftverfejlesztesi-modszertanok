7a05200da6c8eb8d0e3c9fd96180ed74577c61ad
Kis Ábel <abel.kis45682@gmail.com>
2023-09-24 20:44:50 +0200

    Átnevezve main.html -> main.php
    Nem lehet html, ha php kódot is akarunk futtatni benne na...

---
+++ src/mainpage/main.php
@@ -0,0 +1,126 @@
+<!doctype html>
+<html lang="hu">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Tárgy követés</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">  
+    <link rel="stylesheet" href="main.css">
+    
+  </head>
+  <body class="main">
+    <div class="container">
+        <div class="table-container">
+            <table class="felevTable">
+                <thead>
+                    <tr>
+                        <th class="felev">1. Félév</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr><td>1</td></tr>
+                    <tr><td>2</td></tr>
+                    <tr><td>3</td></tr>
+                    <tr><td>4</td></tr>
+                    <tr><td>5</td></tr>
+                    <tr class="deleted-row"><td>6</td></tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="table-container">
+            <table class="felevTable">
+                <thead>
+                    <tr>
+                        <th class="felev">2. Félév</th>
+                    </tr>                
+                </thead>
+                <tbody>
+                    <tr><td>1</td></tr>
+                    <tr><td>2</td></tr>
+                    <tr><td>3</td></tr>
+                    <tr><td>4</td></tr>
+                    <tr><td>5</td></tr>
+                    <tr><td>6</td></tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="table-container">
+            <table class="felevTable">
+                <thead>
+                    <tr>
+                        <th class="felev">3. Félév</th>
+                    </tr>                
+                </thead>
+                <tbody>
+                    <tr><td>1</td></tr>
+                    <tr><td>2</td></tr>
+                    <tr><td>3</td></tr>
+                    <tr><td>4</td></tr>
+                    <tr class="deleted-row"><td>5</td></tr>
+                    <tr class="deleted-row"><td>6</td></tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="table-container">
+            <table class="felevTable">
+                <thead>
+                    <tr>
+                        <th class="felev">4. Félév</th>
+                    </tr>                
+                </thead>
+                <tbody>
+                    <tr><td>1</td></tr>
+                    <tr><td>2</td></tr>
+                    <tr><td>3</td></tr>
+                    <tr><td>4</td></tr>
+                    
+                    
+                </tbody>
+            </table>
+        </div>
+        <div class="table-container">
+            <table class="felevTable">
+                <thead>
+                    <tr>
+                        <th class="felev">5. Félév</th>
+                    </tr>                
+                </thead>
+                <tbody>
+                    <tr><td>1</td></tr>
+                    <tr><td>2</td></tr>
+                    <tr><td>3</td></tr>
+                    <tr class="deleted-row"><td>4</td></tr>
+                    <tr class="deleted-row"><td>5</td></tr>
+                    <tr class="deleted-row"><td>6</td></tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="table-container">
+            <table class="felevTable">
+                <thead>
+                    <tr>
+                        <th class="felev">6. Félév</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr><td>1</td></tr>
+                    <tr class="deleted-row"><td>2</td></tr>
+                    <tr class="deleted-row"><td>3</td></tr>
+                    <tr class="deleted-row"><td>4</td></tr>
+                    <tr class="deleted-row"><td>5</td></tr>
+                    <tr class="deleted-row"><td>6</td></tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <footer class="szamlalo">
+        <div class="row">
+            <div class="col kreditSzamlalo">Itt kredit szamlaló</div>
+            <div class="col tejesitettKredit">Itt teljesített kreditek</div>
+        </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6SpejaU02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
